7c60923ed2f744c5b45f4106a4d460a1f5df32f3
tilakpoudel <tilak.poudel.314@gmail.com>
2022-03-21 22:40:56 +0545

    feat: add items inside drawer

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/utils/routes.dart';
+import '../utils/routes.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
