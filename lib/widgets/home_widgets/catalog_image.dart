50b83b11c6349dbf677df7b426222a5d86a7e0c2
tilakpoudel <tilak.poudel.314@gmail.com>
2022-03-30 22:20:08 +0545

    feat: hero animation

---
+++ lib/widgets/home_widgets/catalog_image.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_catalog/widgets/theme.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class CatlogImage extends StatelessWidget {
+  final String image;
+
+  const CatlogImage({Key? key, required this.image}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(
+      image,
+    ).box.p8.color(MyTheme.creamColor).make().p16().w40(context);
+  }
+}
